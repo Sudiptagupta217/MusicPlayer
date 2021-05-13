c5139990bdd551f572c9991060b9e70e1f1e72e0
Sudipta Gupta <sudiptagupta217@gmail.com>
2021-05-14 00:47:36 +0530

    Set ProgressBar

--- app/src/main/java/com/sudipta/musicplay/MainActivity.java
+++ app/src/main/java/com/sudipta/musicplay/MainActivity.java
@@ -2,30 +2,97 @@ package com.sudipta.musicplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
-
-    //for playing music
-    public void play(View view){
-        mediaPlayer.start();
-    }
-    //for pauseing music
-    public void pause(View view){
-        mediaPlayer.pause();
-    }
-    //for stop music
-    public void stop(View view){
-        mediaPlayer.stop();
-    }
+    AudioManager audioManager;
+    SeekBar seekVol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mediaPlayer = MediaPlayer.create(this,R.raw.music);
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+
+        // Set volume control using seekBar
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        seekVol = findViewById(R.id.volumeSeekBar);
+        seekVol.setMax(maxVol);
+        seekVol.setProgress(curVol);
+
+        //chance volume
+        seekVol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        /////set seekprogress
+        SeekBar seekProg = findViewById(R.id.seekProg);
+        seekProg.setMax(mediaPlayer.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekProg.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 10);
+
+        seekProg.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    //for playing music
+    public void play(View view) {
+        mediaPlayer.start();
+    }
+
+    //for pauseing music
+    public void pause(View view) {
+        mediaPlayer.pause();
+    }
+
+    //for stop music
+    public void stop(View view) {
+        mediaPlayer.stop();
     }
 }
